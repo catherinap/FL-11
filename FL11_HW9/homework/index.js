23288786eafeeb552dbd27df30a571f34e14a3e1
Katerina Panchenko <buddhagolden@gmail.com>
2019-07-15 00:55:08 +0200

    HW9

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,4 +1,3 @@
-'use strict';
 //0_getNumbers
 function getNumbers(str) {
     let arr = str.split('');
@@ -40,11 +39,11 @@ function findTypes(...data) {
                 break;
         }
     }
-   for(let props in dataTypes) { 
-       if(dataTypes[props] === 0) {
-         delete dataTypes[props];  
-       }
-   }
+    for (let props in dataTypes) {
+        if (dataTypes[props] === 0) {
+            delete dataTypes[props];
+        }
+    }
     return dataTypes;
 }
 //2_executeForEach
@@ -73,7 +72,8 @@ function filterArray(arr, func) {
 function showFormattedDate(formatDate) {
     let months = [
         'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']; 
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
     return `Date: ${months[formatDate.getMonth()]} ${formatDate.getDate()} ${formatDate.getFullYear()}`;
 }
 //6_canConvertToDate
@@ -88,45 +88,61 @@ function daysBetween(date1, date2) {
     return Math.round(Math.abs(date2 - date1) / (s * ms * h));
 }
 //8_getAmountOfAdultPeople
-let data = [
-    {
-      '_id': '5b5e3168c6bf40f2c1235cd6',
-      'index': 0,
-      ' birthday ': '2016-03-18T00:00:00',
-      'eyeColor': 'green',
-      'name': 'Stein',
-      'favoriteFruit': 'apple'
+let data = [{
+        '_id': '5b5e3168c6bf40f2c1235cd6',
+        'index': 0,
+        ' birthday ': '2016-03-18T00:00:00',
+        'eyeColor': 'green',
+        'name': 'Stein',
+        'favoriteFruit': 'apple'
     },
     {
-      '_id': '5b5e3168e328c0d72e4f27d8',
-      'index': 1,
-      ' birthday ': '1991-02-11T00:00:00',
-      'eyeColor': 'blue',
-      'name': 'Cortez',
-      'favoriteFruit': 'strawberry'
+        '_id': '5b5e3168e328c0d72e4f27d8',
+        'index': 1,
+        ' birthday ': '1991-02-11T00:00:00',
+        'eyeColor': 'blue',
+        'name': 'Cortez',
+        'favoriteFruit': 'strawberry'
     },
     {
-      '_id': '5b5e3168cc79132b631c666a',
-      'index': 2,
-      ' birthday ': '1984-04-17T00:00:00',
-      'eyeColor': 'blue',
-      'name': 'Suzette',
-      'favoriteFruit': 'apple'
+        '_id': '5b5e3168cc79132b631c666a',
+        'index': 2,
+        ' birthday ': '1984-04-17T00:00:00',
+        'eyeColor': 'blue',
+        'name': 'Suzette',
+        'favoriteFruit': 'apple'
     },
     {
-      '_id': '5b5e31682093adcc6cd0dde5',
-      'index': 3,
-      ' birthday ': '1994-04-17T00:00:00',
-      'eyeColor': 'green',
-      'name': 'George',
-      'favoriteFruit': 'banana'
+        '_id': '5b5e31682093adcc6cd0dde5',
+        'index': 3,
+        ' birthday ': '1994-04-17T00:00:00',
+        'eyeColor': 'green',
+        'name': 'George',
+        'favoriteFruit': 'banana'
     }
-  ] 
+]
 function getAmountOfAdultPeople(data) {
     let age = 18;
     let year = 365;
-    let adultCheck = filterArray(data, person => daysBetween(new Date(), 
-    Date.parse(person[' birthday '])) / year > age).length; 
+    let adultCheck = filterArray(data, person => daysBetween(new Date(),
+        Date.parse(person[' birthday '])) / year > age).length;
     return adultCheck;
 }
 //9_keys
+function key(keys) {
+    let keyArray = [];
+    for (let prop in keys) {
+        if (keys.hasOwnProperty(prop)) {
+            keyArray.push(prop);
+        }
+    }
+}
+//10_values
+function value(values) {
+    let valueArray = [];
+    for (let val in values) {
+        if (values.hasOwnProperty(val)) {
+            valueArray.push(values[val]);
+        }
+    }
+}
