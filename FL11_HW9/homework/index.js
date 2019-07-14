827c81883a9ba017e618b60589f8076e83432562
Katerina Panchenko <buddhagolden@gmail.com>
2019-07-14 05:01:02 +0200

    HW9

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,5 +1,5 @@
 'use strict';
-//getNumbers
+//0_getNumbers
 function getNumbers(str) {
     let arr = str.split('');
     let del = [];
@@ -10,7 +10,7 @@ function getNumbers(str) {
     }
     return del;
 }
-//findTypes
+//1_findTypes
 function findTypes(...data) {
     let dataTypes = {
         'string': 0,
@@ -47,19 +47,19 @@ function findTypes(...data) {
    }
     return dataTypes;
 }
-//executeForEach
+//2_executeForEach
 function executeForEach(arr, func) {
     for (let i = 0; i < arr.length; i++) {
         func(arr[i]);
     }
 }
-//mapArray
+//3_mapArray
 function mapArray(arr, func) {
     let transformedArr = [];
     executeForEach(arr, item => transformedArr.push(func(item)));
     return transformedArr;
 }
-// //filterArray
+//4_filterArray
 function filterArray(arr, func) {
     let filteredArray = [];
     executeForEach(arr, item => {
@@ -69,14 +69,65 @@ function filterArray(arr, func) {
     });
     return filteredArray;
 }
-//showFormattedDate
+//5_showFormattedDate
 function showFormattedDate(formatDate) {
     let months = [
         'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']; 
     return `Date: ${months[formatDate.getMonth()]} ${formatDate.getDate()} ${formatDate.getFullYear()}`;
 }
-//canConvertToDate
+//6_canConvertToDate
 function canConvertToDate(str) {
     return !isNaN(Date.parse(str));
 }
+//7_daysBetween
+function daysBetween(date1, date2) {
+    let s = 3600;
+    let ms = 1000;
+    let h = 24;
+    return Math.round(Math.abs(date2 - date1) / (s * ms * h));
+}
+//8_getAmountOfAdultPeople
+let data = [
+    {
+      '_id': '5b5e3168c6bf40f2c1235cd6',
+      'index': 0,
+      ' birthday ': '2016-03-18T00:00:00',
+      'eyeColor': 'green',
+      'name': 'Stein',
+      'favoriteFruit': 'apple'
+    },
+    {
+      '_id': '5b5e3168e328c0d72e4f27d8',
+      'index': 1,
+      ' birthday ': '1991-02-11T00:00:00',
+      'eyeColor': 'blue',
+      'name': 'Cortez',
+      'favoriteFruit': 'strawberry'
+    },
+    {
+      '_id': '5b5e3168cc79132b631c666a',
+      'index': 2,
+      ' birthday ': '1984-04-17T00:00:00',
+      'eyeColor': 'blue',
+      'name': 'Suzette',
+      'favoriteFruit': 'apple'
+    },
+    {
+      '_id': '5b5e31682093adcc6cd0dde5',
+      'index': 3,
+      ' birthday ': '1994-04-17T00:00:00',
+      'eyeColor': 'green',
+      'name': 'George',
+      'favoriteFruit': 'banana'
+    }
+  ]
+  
+function getAmountOfAdultPeople(data) {
+    let age = 18;
+    let year = 365;
+    let adultCheck = filterArray(data, person => daysBetween(new Date(), 
+    Date.parse(person[' birthday '])) / year > age).length; 
+    return adultCheck;
+}
+//keys
