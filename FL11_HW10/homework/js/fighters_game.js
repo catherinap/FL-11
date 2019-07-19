423b65c1aa1e667b6631ce395fde62f851605569
Katerina Panchenko <buddhagolden@gmail.com>
2019-07-19 17:22:22 +0200

    Fight game JS

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,69 +1,57 @@
-class Fighter {
-    constructor({name, damage, agility}) {
-        this.name = name;
-        this.damage = damage;
-        this.hp = 100;
-        this.agility = agility;
-        this.win = 0;
-        this.loss = 0;
-    }
-    getName() {
-        return this.name;
-    }
-    getDamage() {
-        return this.damage;
-    }
-    getHealth() {
-        return this.hp;
-    }
-    getAgility() {
-        return this.agility;
-    }
-    attack(enemy) {
+function Fighter(fighterObj) {
+    fighterObj.hp = 100;
+    fighterObj.win = 0;
+    fighterObj.loss = 0;
+
+    this.getName = () => fighterObj.name;
+    this.getDamage = () => fighterObj.damage;
+    this.getHealth = () => fighterObj.hp;
+    this.getAgility = () => fighterObj.agility;
+
+    this.attack = function (enemy) {
         let maxSuccess = 100;
         let attackSuccess = maxSuccess - enemy.getAgility();
         let probability = Math.floor(Math.random() * maxSuccess);
         if (probability < attackSuccess) {
             enemy.dealDamage(this.getDamage());
-            console.log(`${this.name} make ${this.damage} damage to ${enemy.name}`);
+            console.log(`${this.getName()} make ${this.getDamage()} damage to ${enemy.getName()}`);
         } else {
-            console.log(`${this.name} attack missed`);
+            console.log(`${this.getName()} attack missed`);
         }
-    }
-    logCombatHistory() {
-        console.log(`Name: ${this.name}, Wins: ${this.win}, Losses: ${this.loss}`);
-    }
-    heal(healPoint) {
-        this.hp = this.hp + healPoint;
-    }
-    dealDamage(damagePoint) {
-        if (this.hp < damagePoint) {
-            this.hp = 0;
+    };
+    this.logCombatHistory = () => {
+        console.log(`Name: ${this.getName()}, Wins: ${this.addWin()}, Losses: ${this.addLoss()}`);
+    };
+    this.heal = function (healPoint) {
+        fighterObj.hp = fighterObj.hp + healPoint;
+        return fighterObj.hp;
+    };
+    this.dealDamage = function (damagePoint) {
+        if (fighterObj.hp < damagePoint) {
+            fighterObj.hp = 0;
         } else {
-            this.hp = this.hp - damagePoint;
+            fighterObj.hp = fighterObj.hp - damagePoint;
         }
-    }
-    addWin() {
-        this.win++;
-    }
-    addLoss() {
-        this.loss++;
-    }
+    };
+    this.addWin = () => fighterObj.win++;
+    this.addLoss = () => fighterObj.loss++;
 }
-const fighter1 = new Fighter({name: 'Tanos', damage: 20, agility: 40});
-const fighter2 = new Fighter({name: 'Iron-man', damage: 40, agility: 20});
+
+const fighter1 = new Fighter({name: 'Tanos', damage: 40, agility: 10});
+const fighter2 = new Fighter({name: 'Iron-man', damage: 20, agility: 50});
+
 function battle(fighter1, fighter2) {
-    if (fighter1.hp === 0) {
-        console.log(`${fighter1.name} is dead and can't fight!`);
-    } else if (fighter2.hp === 0) {
-        console.log(`${fighter2.name} is dead and can't fight!`);
+    if (fighter1.getHealth() === 0) {
+        console.log(`${fighter1.getName()} is dead and can't fight!`);
+    } else if (fighter2.getHealth() === 0) {
+        console.log(`${fighter2.getName()} is dead and can't fight!`);
     } else {
-        while (fighter1.hp > 0 && fighter2.hp > 0) {
+        while (fighter1.getHealth() > 0 && fighter2.getHealth() > 0) {
             fighter1.attack(fighter2);
             fighter2.attack(fighter1);
         }
     }
-    if (fighter1.hp > fighter2.hp) {
+    if (fighter1.getHealth() > fighter2.getHealth()) {
         fighter1.addWin();
         fighter2.addLoss();
     } else {
@@ -71,5 +59,3 @@ function battle(fighter1, fighter2) {
         fighter1.addLoss();
     }
 }
-
-
